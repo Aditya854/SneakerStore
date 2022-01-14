41b31ee9d230c5fbd9f8b7a98b9ac25b2d1f605d
Anmol Singh <anmolsbhinder@gmail.com>
2022-01-14 15:48:24 +0530

    video

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Caraosuel from '../home/Caraousel';
-
+import video1 from "../Resources/SneakPeek/wow1.mp4";
 
 
 function Homepage() {
@@ -9,7 +9,9 @@ function Homepage() {
     <div>
         
         <Caraosuel/>
-       
+        <video className='vv' autoPlay muted controls loop>
+          <source src={video1} type="video/mp4"/>
+          </video>
             
     </div>
    
