2fa2ba715fa994a5b6e01181990722c3b66e59f0
Anmol Singh <anmolsbhinder@gmail.com>
2022-01-15 22:37:13 +0530

    idk

--- src/home/Menu.jsx
+++ src/home/Menu.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+// import SearchIcon from './Search'
 
 function Menu() {
     return (
         <div className='menu'>
-          <div className='menu1'>
+          {/* <div className='menu1'>
             <h3>Men's</h3>
             <p>Shop now</p>
           </div>
@@ -16,7 +17,14 @@ function Menu() {
           <div className='menu3'>
             <h3>Trend</h3>
             <p>Shop now</p>
-          </div>
+          </div> */}
+
+<div className="header_search">
+        <input className="header_searchInput" type="text" />
+        <i class="fas fa-search fa-2x"></i>
+        {/* <SearchIcon className="header_searchIcon" /> */}
+      </div>
+
         </div>
     )
 }
